884438effa89e71b28a138e4138096d74843d21e
Syed Asad Jameel <syedasad1410@gmail.com>
2026-07-29 10:25:18 +0500

    feat: implement global ScrollToTop component and configure React Router in index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
+import ScrollToTop from "./components/ScrollToTop";
 
 import App from "./App";
 import "./index.css";
@@ -9,6 +10,7 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <HelmetProvider>
     <BrowserRouter>
+      <ScrollToTop />
       <App />
     </BrowserRouter>
   </HelmetProvider>
